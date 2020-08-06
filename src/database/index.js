45fe5c5f374fb886df644abc83e966782410c369
GuilhermeGoes <guilhermegoes76@hotmail.com>
2020-08-05 21:15:37 -0300

    edição configuração knex

--- src/database/index.js
+++ src/database/index.js
@@ -1,4 +1,8 @@
-const knex = require('knex')(knexfile.development)
+const knex = require('knex')
+
+const env = process.env.DB_ENV || 'development'
 const knexfile = require('../../knexfile');
 
-module.exports = knex;
+const db = knex(knexfile[env])
+
+module.exports = db;
