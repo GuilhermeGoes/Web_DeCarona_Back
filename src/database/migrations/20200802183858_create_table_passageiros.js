90e67044326aabb3c25a26acdc497dbd3c394d37
GuilhermeGoes <guilhermegoes76@hotmail.com>
2020-08-06 01:58:19 -0300

    correção migrations

--- src/database/migrations/20200802183858_create_table_passageiros.js
+++ src/database/migrations/20200802183858_create_table_passageiros.js
@@ -4,7 +4,7 @@ exports.up = knex => {
         table.increments('id').primary()
         table.string('nome').notNullable()
         table.date('nascimento').notNullable()
-        table.string('cpf', 2).notNullable()
+        table.string('cpf', 11).notNullable()
         table.string('sexo').notNullable()
     })
 };
